13817471eb5c46b0062496dbbe0993881b572f0f
thuynguyet <nguyetnmt22411@st.uel.edu.vn>
2024-10-24 18:51:31 +0700

    Update api.py

--- api.py
+++ api.py
@@ -2,28 +2,28 @@ import requests
 
 # Hàm lấy dữ liệu thời tiết từ OpenWeatherMap API
 def get_weather_data(city_name, api_key):
-   """Lấy dữ liệu thời tiết từ OpenWeatherMap API."""
-   base_url = "http://api.openweathermap.org/data/2.5/weather"
-   params = {
-       'q': city_name,
-       'appid': api_key,
-       'units': 'metric'  # Đơn vị nhiệt độ: Celsius
-   }
+  """Lấy dữ liệu thời tiết từ OpenWeatherMap API."""
+  base_url = "http://api.openweathermap.org/data/2.5/weather"
+  params = {
+      'q': city_name,
+      'appid': api_key,
+      'units': 'metric'  # Đơn vị nhiệt độ: Celsius
+  }
 
-   response = requests.get(base_url, params=params)
-   if response.status_code == 200:
-       data = response.json()
-       return {
-           "city": data["name"],
-           "nation": data["sys"]["nation"],
-           "temperature": data["main"]["temp"],
-           "wind_speed": data["wind"]["speed"],
-           "wind_deg": data["wind"]["deg"],
-           "pressure": data["main"]["pressure"],
-           "humidity": data["main"]["humidity"],
-           "clouds": data["clouds"]["all"],
-           "visibility": data["visibility"] / 1000,  # Convert visibility from meters to km
-           "coordinates": (data["coord"]["lat"], data["coord"]["long"])
-       }
-   else:
-       return None
+  response = requests.get(base_url, params=params)
+  if response.status_code == 200:
+      data = response.json()
+      return {
+          "city": data["name"],
+          "country": data["sys"]["country"],
+          "temperature": data["main"]["temp"],
+          "wind_speed": data["wind"]["speed"],
+          "wind_deg": data["wind"]["deg"],
+          "pressure": data["main"]["pressure"],
+          "humidity": data["main"]["humidity"],
+          "clouds": data["clouds"]["all"],
+          "visibility": data["visibility"] / 1000,  # Convert visibility from meters to km
+          "coordinates": (data["coord"]["lat"], data["coord"]["lon"])
+      }
+  else:
+      return None
