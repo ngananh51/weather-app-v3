22dade397d1d6a72557bdf2624279d4d267eee1a
thuynguyet <nguyetnmt22411@st.uel.edu.vn>
2024-10-29 00:01:46 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,29 +1,32 @@
 import requests
 
-# Hàm lấy dữ liệu thời tiết từ OpenWeatherMap API
-def get_weather_data(city_name, api_key):
-  """Lấy dữ liệu thời tiết từ OpenWeatherMap API."""
-  base_url = "http://api.openweathermap.org/data/2.5/weather"
-  params = {
-      'q': city_name,
-      'appid': api_key,
-      'units': 'metric'  # Đơn vị nhiệt độ: Celsius
-  }
+# API key từ OpenWeatherMap
+API_KEY = '92c553dbdce3e66246fefaad9c9460e5'
 
-  response = requests.get(base_url, params=params)
-  if response.status_code == 200:
-      data = response.json()
-      return {
-          "city": data["name"],
-          "country": data["sys"]["country"],
-          "temperature": data["main"]["temp"],
-          "wind_speed": data["wind"]["speed"],
-          "wind_deg": data["wind"]["deg"],
-          "pressure": data["main"]["pressure"],
-          "humidity": data["main"]["humidity"],
-          "clouds": data["clouds"]["all"],
-          "visibility": data["visibility"] / 1000,  # Convert visibility from meters to km
-          "coordinates": (data["coord"]["lat"], data["coord"]["lon"])
-      }
-  else:
-      return None
+def get_weather_data(city_name, api_key):
+    """Lấy dữ liệu thời tiết từ OpenWeatherMap API."""
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city_name,
+        'appid': API_KEY,
+        'units': 'metric'  # Đơn vị nhiệt độ: Celsius
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "city": data["name"],
+            "country": data["sys"]["country"],
+            "temperature": data["main"]["temp"],
+            "wind_speed": data["wind"]["speed"],
+            "wind_deg": data["wind"]["deg"],
+            "pressure": data["main"]["pressure"],
+            "humidity": data["main"]["humidity"],
+            "clouds": data["clouds"]["all"],
+            "visibility": data["visibility"] / 1000,
+            "coordinates": (data["coord"]["lat"], data["coord"]["lon"])
+        }
+    else:
+        return None
